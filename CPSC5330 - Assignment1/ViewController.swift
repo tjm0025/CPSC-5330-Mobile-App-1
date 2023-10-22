05b56e3b69cacf42796a16cb02aed6a3f863727d
TyannaJ <104778821+tjm0025@users.noreply.github.com>
2023-10-22 10:51:57 -0500

    Add files via upload

---
+++ CPSC5330 - Assignment1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CPSC5330 - Assignment1
+//
+//  Created by Tyanna on 10/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
